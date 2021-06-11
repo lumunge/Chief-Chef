e3f9327a43895990a6f78e746f64534d2acb0310
Erick Lumunge <lumungep12@gmail.com>
2021-06-11 19:31:30 +0300

    final updates

--- src/Components/Meals.jsx
+++ src/Components/Meals.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-// getting meal properties
 const Meals = ({mealId, image, title, origin}) => {
 
     return(
             <div className="meal">
                 <img className="img-fluid" src={image} alt={title} />
                 <p className="font-weight-bold pt-4 text-dark">{origin} - {title}</p>
-                {/* create a route to single meal based on the meal id */}
                 <Link to={{
-                        //assign path as meal/:00345, send state mealId
                         pathname: `/meal/:${mealId}`,
                         state: { meal: mealId}
                     }}> 
