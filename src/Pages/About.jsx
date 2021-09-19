214e11d5980e1cec64051c0404c14e89f1cb51e0
Erick Lumunge <lumungep12@gmail.com>
2021-09-20 00:04:38 +0300

    final build

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 const About = () => {
     return(
         <div className="about">
-            <br/>
-            <br/>
-            <br/>
             <h1>About This App</h1>
             <div>
                 <p>A free web tool that enables you to search for recipes for over 1000+ foods and their ingredients, preparation methods and country of origin.</p>
